2f49cd9ed10f933540ed73c03fac42680f6f7f4d
awsnterraform <awsnterraform@gmail.com>
2017-02-08 00:49:37 -0600

    comments for output file

--- output.tf
+++ output.tf
@@ -1,7 +1,8 @@
+# This is to get the information of public Ip of ec2 machine whichterraform create.
 output "public_ip" {
       value = "${aws_instance.terraform-ec2.public_ip}"
 }
-
+# This is to get the information of private Ip of ec2 machine whichterraform create.
 output "private_ip" {
       value = "${aws_instance.terraform-ec2.private_ip}"
 }
