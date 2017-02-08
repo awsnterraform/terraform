178e556920a82ea1b1bdb15333d8b5b27b2e1e65
awsnterraform <awsnterraform@gmail.com>
2017-02-08 00:43:34 -0600

    comments for security group

--- sg.tf
+++ sg.tf
@@ -1,17 +1,22 @@
+# Create a security group with name terraform-ec2
 resource "aws_security_group" "terraform-ec2" {
         name = "terraform-ec2"
+
+# Allow incoming connection on port 80 for web server
         ingress {
                 from_port = 80
                 to_port = 80
                 protocol = "tcp"
                 cidr_blocks = ["0.0.0.0/0"]
         }
+# Allow SSH access on port 22 for specific IP addresses
         ingress {
                 from_port = 22
                 to_port = 22
                 protocol = "tcp"
-                cidr_blocks = ["74.86.35.77/32","14.141.80.50/32"]
+                cidr_blocks = ["71.xx.xxx.xx/32","14.xx.xxx.1/32"]
 	}
+# Allow outbound traffic to all else you will not able to access internet on machine.
     	egress {
         	from_port       = 0
         	to_port         = 0
